1edfdd7e3be768f54eda7a16e46a1a9db157f62e
Aniket Vishwakarma <aniketvishwakarma569@gmail.com>
2024-03-29 10:57:32 +0530

    modified:   app.py 	modified:   housing/exception/__init__.py 	modified:   housing/logger/__init__.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route('/' , methods = ['GET' , "POST"])
+
+def index():
+    logging.info('we are testing the logging modeulee')
+    return 'CI CD pipline has been established'
+
 
 def index():
     return 'Starting Machine learning project'
