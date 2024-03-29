764214e84f37e75d6c1fc64af12b01ac854133a3
Aniket Vishwakarma <aniketvishwakarma569@gmail.com>
2024-03-29 13:59:56 +0530

    modified:   app.py 	modified:   housing/exception/__init__.py

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from housing.logger import logging
+from housing.exception import housingException
 
 app = Flask(__name__)
 
 @app.route('/' , methods = ['GET' , "POST"])
 
 def index():
-    logging.info('we are testing the logging modeulee')
+    try:
+        raise Exception('we are testing custom exception')
+    
+    except Exception as e:
+        housing = housingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info('we are testing the logging modeulee')
     return 'CI CD pipline has been established'
-
-
-def index():
-    return 'Starting Machine learning project'
 
 
 if __name__ =="__main__":
